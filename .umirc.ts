d1e64ca6b2245ca2d7a6c3e35774ff0944668e50
DelphiTeacher <40187428+DelphiTeacher@users.noreply.github.com>
2022-08-20 16:42:12 +0800

    加入Visual studio code的工程

---
+++ .umirc.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'dumi';
+
+const repo = 'OrangeUI';
+
+export default defineConfig({
+  title: repo,
+  favicon:
+    'http://www.orangeui.cn/image/logo_60.png',
+  logo:
+    'http://www.orangeui.cn/image/logo_60.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  hash: true,
+  // Because of using GitHub Pages
+  base: `/${repo}/`,
+  publicPath: `/${repo}/`,
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/DelphiTeacher/OrangeUI',
+    },
+    {
+      title: 'Blog',
+      path: 'http://www.orangeui.cn/wordpress/',
+    },
+  ],
+  // more config: https://d.umijs.org/config
+
+});
