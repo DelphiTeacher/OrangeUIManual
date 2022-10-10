2e844af7e29385841bd3f82bd3b953b6117b5161
DelphiTeacher <40187428+DelphiTeacher@users.noreply.github.com>
2022-10-10 08:06:01 +0800

    加入了会员入口

--- .umirc.ts
+++ .umirc.ts
@@ -4,16 +4,15 @@ const repo = 'OrangeUI';
 
 export default defineConfig({
   title: repo,
-  favicon:
-    'http://www.orangeui.cn/image/logo_60.png',
-  logo:
-    'http://www.orangeui.cn/image/logo_60.png',
+  favicon: 'http://www.orangeui.cn/image/logo_60.png',
+  logo: 'http://www.orangeui.cn/image/logo_60.png',
   outputPath: 'dist',
   mode: 'site',
   hash: true,
   // Because of using GitHub Pages
-  base: `/${repo}/`,
-  publicPath: `/${repo}/`,
+  base: `/`,
+  publicPath: `/`,
+  exportStatic: {},
   navs: [
     null,
     // {
@@ -32,7 +31,10 @@ export default defineConfig({
       title: 'Blog',
       path: 'http://www.orangeui.cn/wordpress/',
     },
+    {
+      title: 'VIP',
+      path: 'http://www.orangeui.cn/delphi_area_index.html',
+    },
   ],
   // locales: [['zh-CN', '中文'], ['en-US', 'English']],
-  
 });
